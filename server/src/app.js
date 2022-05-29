273e0fdcc8b9d8b4a5c4a1c5d9e286608a3131e7
Asyraf <are.syraf9764@gmail.com>
2022-05-29 15:52:23 +0800

    add axios

--- server/src/app.js
+++ server/src/app.js
@@ -8,13 +8,19 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(morgan('combined'))
-app.use(morgan('combine'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+// app.get('/status', (req, res) => {
+//     res.send({
+//         message: 'hi world'
+//     })
+// })
+
+app.post('/register', (req, res) => {
     res.send({
-        message: 'hi world'
+        message: `Hello  ${req.body.email}, you have been Registered`,
+
     })
 })
 
