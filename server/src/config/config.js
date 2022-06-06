d0082b07f6c250e653d522e87b3f27720b9f219e
Asyraf <are.syraf9764@gmail.com>
2022-06-06 15:45:20 +0800

    change to mysql db

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,13 +1,34 @@
 module.exports = {
+
+    // sqlite3
+    // port: process.env.PORT || 8081,
+    // db : {
+    //     database: process.env.DB_NAME || 'tabtracker',
+    //     user: process.env.DB_USER || 'tabtracker',
+    //     password: process.env.DB_PASS || 'tabtracker',
+    //     options: {
+    //         dialect: process.env.DIALECT || 'mysql',
+    //         host: process.env.HOST || 'localhost',
+    //         storage: './tabtracker.mysql',
+    //     }
+    // },
+    // authentication: {
+    //     jwtSecret: process.env.JWT_SECRET || 'secret'
+    // }
+
+    //mysql
     port: process.env.PORT || 8081,
     db : {
         database: process.env.DB_NAME || 'tabtracker',
-        user: process.env.DB_USER || 'tabtracker',
-        password: process.env.DB_PASS || 'tabtracker',
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASS || '',
         options: {
-            dialect: process.env.DIALECT || 'sqlite',
+            dialect: process.env.DIALECT || 'mysql',
             host: process.env.HOST || 'localhost',
-            storage: './tabtracker.sqlite',
+            // storage: './tabtracker.mysql',
         }
+    },
+    authentication: {
+        jwtSecret: process.env.JWT_SECRET || 'secret'
     }
 }
